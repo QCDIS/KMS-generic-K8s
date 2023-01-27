cb4b83d015de2294b1a6b2ee2c1f24ec106a3c2f
skoulouzis <koulouzis@gmail.com>
2023-01-27 16:55:06 +0100

    organize imports

--- DSS/urls.py
+++ DSS/urls.py
@@ -1,8 +1,6 @@
-from django.conf.urls import include
 from django.urls import re_path as url
-from DSS import views,models
-from django.conf.urls.static import static
 
+from DSS import views
 
 urlpatterns = [
     url(r'^numberOfSolutions', views.numberOfSolutions, name='numberOfSolutions'),
