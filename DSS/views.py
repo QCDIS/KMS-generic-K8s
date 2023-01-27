1d0fd896f51b56685d182c9af486f6a600f07946
skoulouzis <koulouzis@gmail.com>
2023-01-27 16:55:06 +0100

    organize imports

--- DSS/views.py
+++ DSS/views.py
@@ -1,19 +1,20 @@
-import json
-import os
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
-#-------------------------------------------------------------------------------------------------------------
-def listOfSolutions(request):
 
-    return JsonResponse({"hits": numHits,"solutions": {}})
-#-------------------------------------------------------------------------------------------------------------
+# Create your views here.
+# -------------------------------------------------------------------------------------------------------------
+def listOfSolutions(request):
+    return JsonResponse({"hits": numHits, "solutions": {}})
+
+
+# -------------------------------------------------------------------------------------------------------------
 @csrf_exempt
 def numberOfSolutions(request):
+    return JsonResponse({'hits': numHits, "solutions": {}})
 
-    return JsonResponse({'hits': numHits, "solutions":{}})
-#-------------------------------------------------------------------------------------------------------------
+
+# -------------------------------------------------------------------------------------------------------------
 def detailedSolution(request):
-    return JsonResponse({"hits": numHits,"solutions": {}})
-#-------------------------------------------------------------------------------------------------------------
+    return JsonResponse({"hits": numHits, "solutions": {}})
+# -------------------------------------------------------------------------------------------------------------
