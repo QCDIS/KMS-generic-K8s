577caab7f3f82fed91c3ad14245672c6875d0e69
skoulouzis <koulouzis@gmail.com>
2023-01-27 16:55:06 +0100

    organize imports

--- dataset_elastic/indexingPipeline/Synonyms.py
+++ dataset_elastic/indexingPipeline/Synonyms.py
@@ -1,6 +1,7 @@
 import nltk
 from nltk.corpus import wordnet
 
+
 def download_nltk_dependencies_if_needed():
     nltk.data.path.append("/var/www/nltk_data")
     try:
@@ -11,6 +12,7 @@ def download_nltk_dependencies_if_needed():
         nltk.pos_tag(nltk.word_tokenize('foobar'))
     except LookupError:
         nltk.download('averaged_perceptron_tagger', download_dir='/var/www/nltk_data')
+
 
 def get_some_word_synonyms(word):
     word = word.lower()
@@ -26,6 +28,7 @@ def get_some_word_synonyms(word):
             synonyms.append(lemma_name)
     return synonyms
 
+
 def get_all_word_synonyms(word):
     word = word.lower()
     synonyms = []
@@ -40,13 +43,14 @@ def get_all_word_synonyms(word):
                 synonyms.append(lemma_name)
     return synonyms
 
+
 download_nltk_dependencies_if_needed()
 
 
 def getSynonyms(words):
-    lstSynonyms=[]
+    lstSynonyms = []
     for word in words:
-        synonyms=get_some_word_synonyms(word)
+        synonyms = get_some_word_synonyms(word)
         for synonym in synonyms:
             lstSynonyms.append(synonym)
     return lstSynonyms
