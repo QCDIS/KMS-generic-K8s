ca136536c8d6f184a530a644cebb7e59018e89bf
skoulouzis <koulouzis@gmail.com>
2023-01-27 16:49:12 +0100

    added base_path = os.environ.get('BASE_PATH')

--- accountManagement/views.py
+++ accountManagement/views.py
@@ -8,6 +8,7 @@ elasticsearch_username = os.environ.get('ELASTICSEARCH_USERNAME')
 elasticsearch_password = os.environ.get('ELASTICSEARCH_PASSWORD')
 kms_admin_username = os.environ.get('KMS_ADMIN_USERNAME')
 kms_admin_password = os.environ.get('KMS_ADMIN_PASSWORD')
+base_path = os.environ.get('BASE_PATH')
 
 def login(request):
     es = Elasticsearch(elasticsearch_url,http_auth=[elasticsearch_username, elasticsearch_password])
@@ -65,4 +66,4 @@ def login(request):
     }
     result = es.search(index="accountmanagement", body=query_body)
     print(result)
-    return render(request,'login.html',{"username": username, "password":password})
+    return render(request,'login.html',{"username": username, "password":password,"base_path":base_path})
