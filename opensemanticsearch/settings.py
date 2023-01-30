3d9a65611fab9572c251a9db51e8b7f4f27eb8b8
skoulouzis <koulouzis@gmail.com>
2023-01-30 15:01:13 +0100

    run collectstatic

--- opensemanticsearch/settings.py
+++ opensemanticsearch/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
