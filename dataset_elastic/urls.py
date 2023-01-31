758fea38c0ce6d96cb680e0e9276bce8d4d33370
skoulouzis <koulouzis@gmail.com>
2023-01-31 17:35:45 +0100

    remove indexingpipeline

--- dataset_elastic/urls.py
+++ dataset_elastic/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     re_path(r'^result', views.home, name='result'),
     re_path(r'^search', views.search_index, name='search_index'),
     re_path(r'^rest', views.rest, name='rest'),
-    re_path(r'^indexingpipeline', views.indexingpipeline, name='indexingpipeline'),
+    # re_path(r'^indexingpipeline', views.indexingpipeline, name='indexingpipeline'),
     re_path(r'^genericsearch', views.genericsearch, name='genericsearch'),
     re_path(r'^aggregates', views.aggregates, name='aggregates')
 
