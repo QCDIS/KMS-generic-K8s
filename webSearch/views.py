1eefafe5dadf98fe18277059f0070a315c015543
skoulouzis <koulouzis@gmail.com>
2023-01-31 17:36:14 +0100

    remove indexingpipeline

--- webSearch/views.py
+++ webSearch/views.py
@@ -906,11 +906,6 @@ def downloadCart(request):
                       "base_path": base_path
                   }
                   )
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 16-setup-pre-commit-to-protect-from-pushing-passwords
 # -----------------------------------------------------------------------------------------------------------------------
 def getAllfunctionList(request):
     if not 'BasketURLs' in request.session or not request.session['BasketURLs']:
